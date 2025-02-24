957fd3a677ff47278002359052d9b83f181391dd
Deeppatel91 <deeppatel9171@gmail.com>
2025-02-23 22:19:45 -0500

    added serveease name on the browser tab

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import favicon from './assets/image_with_white_bg (1).ico';
 
 import TermsandCondition from './components/TermsandCondition';
 import About from './components/About';
 
 const App = () => {
   const [isAuthenticated] = useState(false);
+
+  useEffect(() => {
+    // Update favicon
+    const linkElement = document.querySelector('link[rel="icon"]');
+    if (linkElement) {
+      linkElement.href = favicon;
+    } else {
+      // If favicon link doesn't exist, create it
+      const newFavicon = document.createElement('link');
+      newFavicon.rel = 'icon';
+      newFavicon.href = favicon;
+      document.head.appendChild(newFavicon);
+    }
+  }, []);
 
   const ProtectedRoute = ({ children }) => {
     return isAuthenticated ? children : <Navigate to="/" replace />;
